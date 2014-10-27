a221419ede7a2dda6f03be39e70be49184126bb9
Jake Scott <jake.net@gmail.com>
2014-10-27 15:34:16 +1300

    Add notification webhook

---
+++ PandaDoc/Models/Webhooks/Notification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PandaDoc.Models.Webhooks
+{
+    public class Data
+    {
+        /// <summary>
+        /// DOCUMENT_UUID
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+
+    public class Notification
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("triggered_at")]
+        public DateTime TriggeredAt { get; set; }
+    }
+}
